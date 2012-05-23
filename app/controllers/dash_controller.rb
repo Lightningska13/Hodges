4f3b317683b5a89d210b6a66ffe61f6f04774ce1
Patrick Lighthall <patrick.lighthall@gmail.com>
2012-05-23 09:03:23 -0400

    Modified Dash Controller

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -2,6 +2,6 @@ class DashController < ApplicationController
   def index
     @page_title= "Hand Crafted Fine Jewelry"
     @event= Event.find_for_home('Showcase', 'desc').first
-    @showpic = @event.eventpic_file_name
+    @showpic = @event.eventpic_file_name unless @event.nil?
   end  
 end
